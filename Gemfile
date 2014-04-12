47dfae90925402ec4d6c82f1e0b7574985ef67ec
Prescott Murphy <prurph@gmail.com>
2014-04-12 15:37:03 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.3'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+
+group :test, :development do
+  gem 'rspec'
+  gem 'rspec-rails', '~> 2.0'
+  gem 'factory_girl_rails', '~> 4.0'
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'selenium-webdriver'
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Receta::Application.config.secret_key_base = '0105b49336d312f996a62be59f8aa5dcd188a732d072bd779654d908d3929a505116ab9e1c5f3f7c935da0628f5b94afc5e05183ae22d7a4c3cbb3fd935c6b4b'
